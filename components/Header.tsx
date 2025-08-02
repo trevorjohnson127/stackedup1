11f3b802a61d886d10532d07bdb2abff8b39fa8b
Trevor Johnson <trevorjohnson127@gmail.com>
2025-08-01 19:18:42 -0500

    Initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+  <header className="p-4 shadow-md bg-white">
+    <h1 className="text-xl font-bold">StackedUp</h1>
+  </header>
+);
+
+export default Header;
